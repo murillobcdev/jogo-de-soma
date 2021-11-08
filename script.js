35fddc070cd1765c96b0cecd8077e15c1d7bb0b4
Murillo Brito <murillobdev@gmail.com>
2021-11-08 11:58:28 -0300

    Score Adicionado

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let acertos = 0;
+let erros = 0;
+
 document.addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
         resultado();
@@ -10,11 +13,27 @@ document.addEventListener('keydown', function (e) {
     }
 }, false);
 
+function verificar() {
+    let r1 = Math.floor(Math.random() * 25);
+    let r2 = Math.floor(Math.random() * 25);
+
+    document.querySelector(".resultadoUsuario").value = "";
+    document.querySelector(".resultadoUsuario").classList.remove("acertou", "errou", "campoVazio");
+    document.querySelector("span").innerHTML = "Some os dois números, coloque no campo e aperte Enter!";
+    document.querySelector("span").classList.remove("titulo");
+    document.querySelector(".resultadoUsuario").focus();
+
+    document.querySelector(".numero1").innerHTML = r1;
+    document.querySelector(".numero2").innerHTML = r2;
+}
+
+verificar();
+
 function resultado() {
-    var n1 = parseInt(document.querySelector(".numero1").innerHTML);
-    var n2 = parseInt(document.querySelector(".numero2").innerHTML);
-    var somaRes = n1 + n2;
-    var r = parseInt(document.querySelector(".resultadoUsuario").value);
+    let n1 = parseInt(document.querySelector(".numero1").innerHTML);
+    let n2 = parseInt(document.querySelector(".numero2").innerHTML);
+    let somaRes = n1 + n2;
+    let r = parseInt(document.querySelector(".resultadoUsuario").value);
 
     if (isNaN(r)) {
         document.querySelector(".resultadoUsuario").classList.add("campoVazio");
@@ -30,28 +49,16 @@ function testarResultado(r, somaRes) {
     if (r == somaRes) {
         document.querySelector(".resultadoUsuario").classList.remove("errou", "campoVazio");
         document.querySelector(".resultadoUsuario").classList.add("acertou");
-        console.log(`${somaRes} é igual a ${r}.`);
+        console.log(`${somaRes} é igual a ${r}`);
+        acertos++;
+        document.querySelector(".acertos").innerHTML = `Acertos: ${acertos}`;
         setTimeout(verificar, 1000);
     } else {
         document.querySelector(".resultadoUsuario").classList.remove("acertou", "campoVazio");
         document.querySelector(".resultadoUsuario").classList.add("errou");
-        console.log(`${somaRes} não é igual a ${r}.`);
+        console.log(`${somaRes} não é igual a ${r}`);
+        erros++;
+        document.querySelector(".erros").innerHTML = `Erros: ${erros}`;
         setTimeout(verificar, 1000);
     }
 }
-
-function verificar() {
-
-    document.querySelector(".resultadoUsuario").value = "";
-    document.querySelector(".resultadoUsuario").classList.remove("acertou", "errou", "campoVazio");
-    document.querySelector("span").innerHTML = "Some os dois números, coloque no campo e aperte Enter!";
-    document.querySelector("span").classList.remove("titulo");
-    document.querySelector(".resultadoUsuario").focus();
-
-    var r1 = Math.floor(Math.random() * 25);
-    var r2 = Math.floor(Math.random() * 25);
-
-    document.querySelector(".numero1").innerHTML = r1;
-    document.querySelector(".numero2").innerHTML = r2;
-}
-verificar();
